bb8d16ebdf33b09adcb6f4adadab746be4977ed9
antasantony <antasantony03@gmail.com>
2026-04-20 18:22:58 +0530

    feat: implement student delete and edit functionality

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -24,8 +24,51 @@ router.post("/signup", (req, res) => {
       return res.send("Error");
     }
 
-    res.send("Signup successful");
+    res.redirect("/students");
   });
 });
 
+router.get("/students", (req, res) => {
+  db.query("SELECT * FROM students", (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.send('Error fetching Students');
+    }
+    res.render("students", { students: result });
+  })
+})
+router.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("DELETE FROM students WHERE id = ?", [id], (err) => {
+    if (err) {
+      console.log(err);
+      return res.send("Error deleting student");
+    }
+
+    res.redirect("/students");
+  });
+});
+router.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("SELECT * FROM students WHERE id = ?", [id], (err, result) => {
+    if (err) return res.send("Error");
+
+    res.render("editStudent", { student: result[0] });
+  });
+});
+router.post("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, email, password } = req.body;
+
+  const sql =
+    "UPDATE students SET name = ?, email = ?, password = ? WHERE id = ?";
+
+  db.query(sql, [name, email, password, id], (err) => {
+    if (err) return res.send("Error updating");
+
+    res.redirect("/students");
+  });
+});
 module.exports = router;
